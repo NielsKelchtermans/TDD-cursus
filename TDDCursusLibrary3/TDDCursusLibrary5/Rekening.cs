da2a0d2bb2a85ca5adc5018d2b669b7e91c930dd
Niels Kelchtermans <kelchtermansniels@gmail.com>
2020-01-15 13:02:52 +0100

    rekening met test toegevoegd

---
+++ TDDCursusLibrary3/TDDCursusLibrary5/Rekening.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDDCursusLibrary
+{
+    public class Rekening
+    {
+        private decimal saldoValue;
+        public void Storten(decimal bedrag)
+        {
+            // throw new NotImplementedException();
+            Saldo += bedrag;
+        }
+        public decimal Saldo
+        {
+            get
+            {
+                //throw new NotImplementedException();
+                return saldoValue;
+            }
+            set
+            {
+                saldoValue = value;
+            }
+        }
+    }
+}
