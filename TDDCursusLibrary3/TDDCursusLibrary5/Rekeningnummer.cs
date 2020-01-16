582f11d17e3c58ef5cc693b4125a4f17b2df9675
Niels Kelchtermans <kelchtermansniels@gmail.com>
2020-01-16 14:20:29 +0100

    Class Rekeningnummer uitgewerkt

--- TDDCursusLibrary3/TDDCursusLibrary5/Rekeningnummer.cs
+++ TDDCursusLibrary3/TDDCursusLibrary5/Rekeningnummer.cs
@@ -1,20 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace TDDCursusLibrary
 {
     public class Rekeningnummer
     {
+        //Regex met stukje hoe rekeningnummer er moet uitzien
+        private static readonly Regex regex = new Regex("^\\d{3}-\\d{7}-\\d{2}$"); //using regularexpressions zie boven
         //constructor
         public Rekeningnummer(string nummer)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            if (!regex.IsMatch(nummer))    //regex heeft een IsMatch method
+            {
+                throw new ArgumentException();
+            }
+            var eerste10cijfers = long.Parse(nummer.Substring(0, 3) + nummer.Substring(4, 7));
+            var laatste2cijfers = long.Parse(nummer.Substring(12, 2));
+
+            if (eerste10cijfers %97L !=laatste2cijfers)
+            {
+                throw new ArgumentException();
+            }
+            this.nummer = nummer;
+            
         }
+        private readonly string nummer;
+
         //method override
         public override string ToString()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return nummer;
         }
     }
 }
