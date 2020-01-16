5325b19d973fb446d100361372fa02953c2da4d0
Niels Kelchtermans <kelchtermansniels@gmail.com>
2020-01-16 14:02:02 +0100

    Grenswaarden en extreme waarden rekeningnummer

---
+++ TDDCursusLibrary3/TDDCursusLibrary5/Rekeningnummer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDDCursusLibrary
+{
+    public class Rekeningnummer
+    {
+        //constructor
+        public Rekeningnummer(string nummer)
+        {
+            throw new NotImplementedException();
+        }
+        //method override
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
