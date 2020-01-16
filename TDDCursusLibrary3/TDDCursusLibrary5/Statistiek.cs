08c1351c3cc8c79dcee4d892073e4456e6dc69d1
Niels Kelchtermans <kelchtermansniels@gmail.com>
2020-01-16 14:39:33 +0100

    Statistiek test class af

---
+++ TDDCursusLibrary3/TDDCursusLibrary5/Statistiek.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDDCursusLibrary
+{
+    public static class Statistiek
+    {
+        //static method
+        public static decimal Gemiddelde(decimal[] getallen)
+        {
+            //throw new NotImplementedException();
+
+        }
+    }
+}
---
+++ TDDCursusLibrary3/TDDCursusLibraryTest5/StatistiekTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TDDCursusLibrary;
+
+namespace TDDCursusLibraryTest
+{
+    [TestClass]
+    public class StatistiekTest
+    {
+        [TestMethod]
+        public void HetGemiddeldeVan10en15is12punt5()
+        {
+
+            Assert.AreEqual(12.5m, Statistiek.Gemiddelde(new decimal[] { 10, 15 }));
+        }
+        [TestMethod]
+        public void HetGemiddeldeVanEenGetalIsDatGetal()
+        {
+            var getal = 1.23m;
+            Assert.AreEqual(getal, Statistiek.Gemiddelde(new decimal[] { getal }));
+        }
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void HetGemiddeldeVanEenLegeVerzamelingKanJeNietBerekenen()
+        {
+            Statistiek.Gemiddelde(new decimal[] { });
+        }
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void HetGemiddeldeVanNullKanJeNietBerekenen()
+        {
+            Statistiek.Gemiddelde(null);
+        }
+
+    }
+
+}
